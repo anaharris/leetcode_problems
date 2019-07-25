9355b5daffa9be77dfd1c48a83cfd646fac998b1
Ana Harris <vana1990@gmail.com>
2019-07-25 14:42:17 -0400

    binary tree

--- binary-tree.rb
+++ binary-tree.rb
@@ -16,14 +16,15 @@
 
 def preorder_traversal(root)
   return [] if root.nil?
-  # create a stack and add root to it
+  # create a stack and add the argument root to it
   stack = [root]
   results = []
-  # until stack is empty, take the first element from stack and add it to results
+  # until stack is full, take the first element from stack and add it to results
   until stack.empty?
     current_node = stack.pop
+    # add value of the current node to results
     results.push current_node.val
-  # add right node to the stack, if it exists, and/or left node to the stack, if it exists
+    # add current node's left and right children to the stack
     stack.push current_node.right if current_node.right
     stack.push current_node.left if current_node.left
   end
