4fe9ae2b6f5b3ef40a92df9e976ce0891a52a477
Ana Harris <vana1990@gmail.com>
2019-08-05 17:46:31 -0400

    m

--- interview-prep-easy-array.rb
+++ interview-prep-easy-array.rb
@@ -132,3 +132,30 @@ def contains_duplicate(nums)
   end
   false
 end
+
+# 5. Single Number
+#
+# Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+#
+# Note:
+#
+# Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+#
+# Example 1:
+#
+# Input: [2,2,1]
+# Output: 1
+# Example 2:
+#
+# Input: [4,1,2,1,2]
+# Output: 4
+#
+# Solution:
+
+def single_number(nums)
+  count_nums = {}
+  nums.each do |n|
+    count_nums[n] ? count_nums[n] +=1 : count_nums[n] = 1
+  end
+  count_nums.key(1)
+end
