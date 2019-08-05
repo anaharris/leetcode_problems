764ce0347ef87cb4c17156945db1ce8741b50146
Ana Harris <vana1990@gmail.com>
2019-08-05 17:43:09 -0400

    m

--- interview-prep-easy-array.rb
+++ interview-prep-easy-array.rb
@@ -52,5 +52,83 @@ end
 # Solution:
 
 def max_profit(prices)
-  
+  profit = 0
+
+  prices.each_with_index do |p, i|
+      if prices[i + 1] && p < prices[i + 1]
+          profit += (prices[i + 1] - p)
+      end
+  end
+
+  profit
+end
+
+# 3. Rotate Array
+#
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+#
+# Example 1:
+#
+# Input: [1,2,3,4,5,6,7] and k = 3
+# Output: [5,6,7,1,2,3,4]
+# Explanation:
+# rotate 1 steps to the right: [7,1,2,3,4,5,6]
+# rotate 2 steps to the right: [6,7,1,2,3,4,5]
+# rotate 3 steps to the right: [5,6,7,1,2,3,4]
+# Example 2:
+#
+# Input: [-1,-100,3,99] and k = 2
+# Output: [3,99,-1,-100]
+# Explanation:
+# rotate 1 steps to the right: [99,-1,-100,3]
+# rotate 2 steps to the right: [3,99,-1,-100]
+# Note:
+#
+# Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+# Could you do it in-place with O(1) extra space?
+#
+# Solution 1:
+
+def rotate(nums, k)
+  k.times do
+    temp = nums.pop
+    nums.unshift(temp)
+  end
+  nums
+end
+
+# Solution 2:
+
+def rotate(nums, k)
+  nums.rotate!(-k)
+end
+
+# 4.  Contains Duplicate
+#
+# Given an array of integers, find if the array contains any duplicates.
+#
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+#
+# Example 1:
+#
+# Input: [1,2,3,1]
+# Output: true
+# Example 2:
+#
+# Input: [1,2,3,4]
+# Output: false
+# Example 3:
+#
+# Input: [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+#
+# Solution:
+
+def contains_duplicate(nums)
+  return false if nums.empty?
+  map = {}
+  nums.each do |n|
+    map[n] ? (return true) : map[n] = 1
+  end
+  false
 end
