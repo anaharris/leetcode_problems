016da4981586f7a91b0161842d10f8ed96e7a6b5
Ana Harris <vana1990@gmail.com>
2019-08-05 17:42:02 -0400

    m

--- best_time_to_buy_and_sell_stock_2.rb
+++ best_time_to_buy_and_sell_stock_2.rb
@@ -66,8 +66,48 @@ end
 # Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
 # Could you do it in-place with O(1) extra space?
 #
-# Solution:
+# Solution 1:
 
 def rotate(nums, k)
+  k.times do
+    temp = nums.pop
+    nums.unshift(temp)
+  end
+  nums
+end
 
+# Solution 2:
+
+def rotate(nums, k)
+  nums.rotate!(-k)
+end
+
+# 4.  Contains Duplicate
+#
+# Given an array of integers, find if the array contains any duplicates.
+#
+# Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+#
+# Example 1:
+#
+# Input: [1,2,3,1]
+# Output: true
+# Example 2:
+#
+# Input: [1,2,3,4]
+# Output: false
+# Example 3:
+#
+# Input: [1,1,1,3,3,4,3,2,4,2]
+# Output: true
+#
+# Solution:
+
+def contains_duplicate(nums)
+  return false if nums.empty?
+  map = {}
+  nums.each do |n|
+    map[n] ? (return true) : map[n] = 1
+  end
+  false
 end
