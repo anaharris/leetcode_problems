ca20cf735c850a2bb88706cce4861c41a84d6471
Ana Harris <vana1990@gmail.com>
2019-08-05 16:34:23 -0400

    arrays

---
+++ interview-prep-easy-array.rb
@@ -0,0 +1,56 @@
+# 1. Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+#
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+#
+# Example 1:
+#
+# Given nums = [1,1,2],
+#
+# Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+#
+# It doesn't matter what you leave beyond the returned length.
+# Example 2:
+#
+# Given nums = [0,0,1,1,1,2,2,3,3,4],
+#
+# Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+#
+# It doesn't matter what values are set beyond the returned length.
+#
+# Solution 1:
+
+def remove_duplicates(nums)
+  nums.uniq!
+  return nums.length
+end
+
+# 2. Say you have an array for which the ith element is the price of a given stock on day i.
+#
+# Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+#
+# Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+#
+# Example 1:
+#
+# Input: [7,1,5,3,6,4]
+# Output: 7
+# Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+#              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+# Example 2:
+#
+# Input: [1,2,3,4,5]
+# Output: 4
+# Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+#              Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are
+#              engaging multiple transactions at the same time. You must sell before buying again.
+# Example 3:
+#
+# Input: [7,6,4,3,1]
+# Output: 0
+# Explanation: In this case, no transaction is done, i.e. max profit = 0.
+#
+# Solution:
+
+def max_profit(prices)
+  
+end
