6be852fc72f7d35b564af25db5b84e2390d79b0a
Ana Harris <vana1990@gmail.com>
2019-08-13 15:35:17 -0400

    tree problem

--- interview-prep-easy-trees.rb
+++ interview-prep-easy-trees.rb
@@ -28,9 +28,97 @@
 #     end
 # end
 
-def max_depth(root)i
+def max_depth(root)
   return 0 if root.nil?
   depth_l = max_depth(root.right) + 1
   depth_r = max_depth(root.left) + 1
   depth_l > depth_r ? depth_l : depth_r
+end
+
+# 2. Validate Binary Search Tree
+#
+# Given a binary tree, determine if it is a valid binary search tree (BST).
+#
+# Assume a BST is defined as follows:
+#
+# The left subtree of a node contains only nodes with keys less than the node's key.
+# The right subtree of a node contains only nodes with keys greater than the node's key.
+# Both the left and right subtrees must also be binary search trees.
+#
+#
+# Example 1:
+#
+#     2
+#    / \
+#   1   3
+#
+# Input: [2,1,3]
+# Output: true
+#
+# Example 2:
+#
+#     5
+#    / \
+#   1   4
+#      / \
+#     3   6
+#
+# Input: [5,1,4,null,null,3,6]
+# Output: false
+# Explanation: The root node's value is 5 but its right child's value is 4.
+#
+# Solution:
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+def is_valid_bst(root)
+
+end
+
+# 3. Symmetric Tree
+#
+# Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+#
+# For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+#
+#     1
+#    / \
+#   2   2
+#  / \ / \
+# 3  4 4  3
+#
+#
+# But the following [1,2,2,null,3,null,3] is not:
+#
+#     1
+#    / \
+#   2   2
+#    \   \
+#    3    3
+#
+#
+# Note:
+# Bonus points if you could solve it both recursively and iteratively.
+#
+# Solution:
+
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val)
+#         @val = val
+#         @left, @right = nil, nil
+#     end
+# end
+
+
+def is_symmetric(root)
+
 end
